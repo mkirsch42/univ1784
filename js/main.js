1083d2592036dadb90e3dd644598c74cff2c2256
Mathew Kirschbaum <mkirsch42@gmail.com>
2017-11-29 22:47:55 -0500

    Remove "Tick" text from components array after hiding it

--- js/main.js
+++ js/main.js
@@ -10,7 +10,9 @@ function FlashText(str, x, y, c, f, frames) {
         if(this.frames-- > 0) {
             ctx.font = this.f;
             ctx.fillStyle = this.c;
-            ctx.fillText(str, this.x, this.y)
+            ctx.fillText(str, this.x, this.y);
+        } else {
+            return true;
         }
     }
 }
@@ -21,24 +23,28 @@ var light_clock = {
     interval: null,
     frame: null,
     components: [],
+    nextgen: [],
     tick: function() {
         this.frame++;
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        $.each(this.components, (idx, comp)=>{
-            comp.tick(this.frame);
-            comp.paint(this.context);
-        });
+        this.components = $.grep(this.components, (comp, idx)=>{
+            let r = comp.tick(this.frame);
+            r |= comp.paint(this.context);
+            return !r;
+        }).concat(this.nextgen);
+        this.nextgen = [];
     },
     start: function() {
         this.interval = setInterval(this.tick, 20);
     },
-    flash: function(receive) {
+    flash: async function(receive) {
         if(receive) {
-            this.components.push(new FlashText("Tick", 110, 210, "dark-red", "16pt Arial", 15));
+            while(this.lock);
+            this.nextgen.push(new FlashText("Tick", 37, 145, "darkred", "16pt Arial", 15));
         }
     },
     init: function () {
-        this.components.push(new LightClock(50, 50, 150, 50, 2, "red", this.flash.bind(this)));
+        this.components.push(new LightClock(0, 0, 150, 50, 2, "red", this.flash.bind(this)));
         this.context = this.canvas.getContext("2d");
         this.frame = 0;
         this.tick = this.tick.bind(this);
